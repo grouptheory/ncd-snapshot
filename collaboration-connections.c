cf54fbc534824ba718ba77d19c7e1de8580b4241
code <code@cool.home>
2011-03-20 13:56:11 -0400

    test

--- collaboration-connections.c
+++ collaboration-connections.c
@@ -170,6 +170,7 @@ void getTabledata(MYSQL *connread, FILE *outfile, char *table, int time)
 		image_count = atoi(row[0]);
 	}
 	else { printf("Error getting count of images.\n"); exit(1); }
+	while( (row = mysql_fetch_row(res)) != NULL);
 	
 	printf("Creating matrix for %d images.\n", image_count);
 	connections = (short int**) malloc (image_count+1 * sizeof(short int));
@@ -177,13 +178,14 @@ void getTabledata(MYSQL *connread, FILE *outfile, char *table, int time)
 	for (x = 0; x <= image_count; x++)
 		connections[x] = (short int*) malloc(image_count+1*sizeof(short int));
    
-   
+   /*
 	for (x=0; x<=image_count; x++) {
 		for(y=0; y<=image_count; y++)
 		{
 			connections[x][y] = 0;
 		}
 	}
+	*/
 	/*
 	//Fill Array
 	printf("Populating adjacency matrix.\n");
